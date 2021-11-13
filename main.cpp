b6216afb8bf70f887fcb30cac2b9c00557691020
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-13 13:57:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main(){
+    int a=0;
+    int b=1;
+    int c=0;
+    int d=0;
+    int n=0;
+    cout << "Inserisci un numero maggiore/uguale a 2: " << endl;
+    cin >> n;
+    if (n<2){
+        cout << "Errore! Inserire un numero maggiore/uguale a 2!" << endl;
+    } else if (n>=2) {
+        cout << "Di seguito al numero che hai scelto: " << n << ", ecco i primi numeri della serie:" << endl;
+        for (d=0; d<=n;d++){
+            c = a+b;
+            cout << c << endl;
+            a = b;
+            b = c;
+        }
+    }
+    return 0;
 }
